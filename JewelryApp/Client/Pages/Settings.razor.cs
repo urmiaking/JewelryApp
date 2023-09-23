61be70d842f5fc141f1500781549f89ed016d212
Masoud - Company <masoud.xpress@gmail.com>
2023-09-23 19:31:55 +0330

    ChangePass Component

--- JewelryApp/Client/Pages/Settings.razor.cs
+++ JewelryApp/Client/Pages/Settings.razor.cs
@@ -7,21 +7,17 @@ namespace JewelryApp.Client.Pages;
 
 public partial class Settings
 {
-    MudListItem selectedItem;
-    object selectedValue = 1;
-    Color _color = Color.Success;
-
     [Parameter]
     public ChangePasswordDto PasswordModel { get; set; } = new();
-
-    protected override async Task OnInitializedAsync()
-    {
-        await base.OnInitializedAsync();
-    }
 
     async Task OnPasswordChangeSubmit()
     {
         await PutAsync("/changepassword", PasswordModel);
+    }
+
+    private void Close()
+    {
+        
     }
 }
 
